e89bea12c9b5edb70eb5efdb5d22ee8806315808
DiegooRibeiroo <42984345+DiegooRibeiroo@users.noreply.github.com>
2018-09-11 21:27:38 -0300

    Update test_teste.py

--- test_teste.py
+++ test_teste.py
@@ -3,6 +3,7 @@ from calc import soma,sub,multi,div
 def test_app():
  assert soma(1,1) == 2
  assert soma(-1,1) == 0
+ assert soma('-1',1) == 0
  assert sub(1.5,1) == 0.5
  assert multi(-1,1) == -1
  assert multi(3e2,2) == 600
